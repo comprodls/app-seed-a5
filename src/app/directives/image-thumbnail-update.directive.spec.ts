b2a9473eb5494b17918512549771270d908abeec
Yamini07 <yamini.chhabra@comprotechnologies.com>
2021-05-12 15:26:19 +0530

    Empty the repo

--- src/app/directives/image-thumbnail-update.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ImageThumbnailUpdateDirective } from './image-thumbnail-update.directive';
-
-describe('ImageThumbnailUpdateDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImageThumbnailUpdateDirective();
-    expect(directive).toBeTruthy();
-  });
-});
